a43bf01c1841e111faaae680143a8c2d0f624cb8
sambhav6496 <jainsam620@gmail.com>
2021-01-25 21:21:49 +0530

    tic tac toe

--- index1.js
+++ index1.js
@@ -1,81 +1,95 @@
-var options = $(".option")
-var option = $(".option")
-var boxes = $(".boxes")
-var box = $('.box')
-var selectSides = $('.select-sides')
+var options = $(".option");
+var option = $(".option");
+var boxes = $(".boxes");
+var box = $(".box");
+var selectSides = $(".select-sides");
+var resultText = $(".result-text");
+var resultBox = $('.result')
+var replay = $(".refresh");
 var count = 0;
-var inputValue = ""
-var playerInput =["" , "" , "" , "" , "" , "", "", "",""]
+var inputValue = "";
+var playerInput = ["", "", "", "", "", "", "", "", ""];
 
-const b0 = $('#0')
-const b1 = $('#1')
-const b2 = $('#2')
-const b3 = $('#3')
-const b4 = $('#4')
-const b5 = $('#5')
-const b6 = $('#6')
-const b7 = $('#7')
-const b8 = $('#8')
-
+// const b0 = $("#0");
+// const b1 = $("#1");
+// const b2 = $("#2");
+// const b3 = $("#3");
+// const b4 = $("#4");
+// const b5 = $("#5");
+// const b6 = $("#6");
+// const b7 = $("#7");
+// const b8 = $("#8");
 
 const win = [
-    [0 , 1 , 2],
-    [3 , 4 , 5],
-    [6 , 7 , 8],
-    [0 , 3 , 6],
-    [1 , 4 , 7],
-    [2 , 5 , 8],
-    [0 , 4 , 8],
-    [2 , 4 , 6],
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-option.click(function(){
-    selectSides.addClass('hidden')
-    boxes.removeClass('hidden')
-    const optionId = this.id
-    removeClick(option)
-    playTurn(optionId)
-})
+option.click(function () {
+  selectSides.addClass("hidden");
+  boxes.removeClass("hidden");
+  const optionId = this.id;
+  removeClick(option);
+  playTurn(optionId);
+});
 
-function playTurn(xOrO){
-    box.click(function(){
-        $(this).html(`<span>${xOrO}</span>`)
-        playerInput[this.id] = xOrO
-        inputValue = xOrO
-        removeClick(this)
-        if(xOrO === "X"){
-            xOrO = "O"
-        }else{
-            xOrO = "X"
-        }
-        count++
-        if(count > 4){
-            checkResult(win , playerInput)
-            }
-    })
-    
-}
-
-function checkResult(winCondition ,receivedInput){
-    for(let i=0; i<winCondition.length; i++){
-        let winner = winCondition[i]
-        let x = receivedInput[winner[0]]
-        let y = receivedInput[winner[1]]
-        let z = receivedInput[winner[2]]
-        if(count < 9){
-            if(x === y && y === z && x !== ""){
-                console.log(`Winner is ${inputValue}`)
-                removeClick(box)
-            }
-        }else{
-            if(x !== y || y !== z)
-            console.log('draw')
-        }
-        
+function playTurn(xOrO) {
+  box.click(function () {
+    $(this).html(`<span>${xOrO}</span>`);
+    playerInput[this.id] = xOrO;
+    inputValue = xOrO;
+    removeClick(this);
+    if (xOrO === "X") {
+      xOrO = "O";
+    } else {
+      xOrO = "X";
     }
+    count++;
+    if (count > 4) {
+      checkResult(win, playerInput);
+    }
+  });
 }
 
+function checkResult(winCondition, receivedInput) {
+  for (let i = 0; i < winCondition.length; i++) {
+    let winner = winCondition[i];
+    let x = receivedInput[winner[0]];
+    let y = receivedInput[winner[1]];
+    let z = receivedInput[winner[2]];
+    if (count < 9) {
+      if (x === y && y === z && x !== "") {
+        removeClick(box);
+        setTimeout(function(){showResult("winner")},200);
+        
+      }
+    }
+  }
+  if (count === 9) {
+    removeClick(box);
+    setTimeout(function(){showResult("draw")},500);
+  }
+}
 
-function removeClick(value){
-    $(value).off('click')
+function showResult(result) {
+  boxes.addClass('hidden');
+  resultBox.removeClass("hidden");
+  replay.click(function(){
+    location.reload()
+  })
+  if (result === "winner") {
+    $(resultText).html(`Winner is ${inputValue}`);
+  }else{
+    $(resultText).html("Draw")
+  }
+}
+
+function removeClick(value) {
+  $(value).off("click");
 }
